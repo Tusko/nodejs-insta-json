8fda9292e540fd472c886dc4efa0c4d3434122f0
Tusko <tusko@photoinside.me>
2019-09-28 13:57:06 +0300

    added netlify-lambda

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const serverless = require("serverless-http");
 const path = require("path");
 const eventHandler = require("./eventHandler");
 
@@ -23,3 +24,6 @@ app
     console.log(`Server is listening on http://localhost:${app.get("port")} `);
     return false;
   });
+
+module.exports = app;
+module.exports.handler = serverless(app);
