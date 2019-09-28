603fc29bc82bd5292228a2a1524e6b9c8ef3db82
Tusko <tusko@photoinside.me>
2019-09-28 15:57:33 +0300

    f

--- express/server.js
+++ express/server.js
@@ -1,23 +1,20 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const serverless = require("serverless-http");
 const path = require("path");
 const eventHandler = require("../eventHandler");
-
 const app = express();
-const router = express.Router();
-
-router
-  .get("/", (req, res) => eventHandler.processing(req, res))
-  .get("/render", (req, res) => {
-    res.sendFile(path.join(`${__dirname}/test/render.html`));
-  });
 
 app
-  .set("port", process.env.PORT || 8182)
   .use(express.static("test"))
-  .use("/.netlify/functions/server", router)
-  .use(bodyParser.json());
+  .set("port", process.env.PORT || 8182)
+  .get("/", (req, res, next) => {
+    eventHandler.processing(req, res);
+  })
+  .get("/render", (req, res, next) => {
+    res.sendFile(path.join(__dirname, `../test/render.html`));
+  });
+
+// app.use("/.netlify/functions/server", router);
 
 module.exports = app;
 module.exports.handler = serverless(app);
